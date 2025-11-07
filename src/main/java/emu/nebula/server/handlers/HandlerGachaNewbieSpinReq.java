b08ad1b7a11778a766b2bccc522caa22681729a3
Melledy <121644117+Melledy@users.noreply.github.com>
2025-11-07 05:43:31 -0800

    Disable newbie gacha spin handler

--- src/main/java/emu/nebula/server/handlers/HandlerGachaNewbieSpinReq.java
+++ src/main/java/emu/nebula/server/handlers/HandlerGachaNewbieSpinReq.java
@@ -2,12 +2,7 @@ package emu.nebula.server.handlers;
 
 import emu.nebula.net.NetHandler;
 import emu.nebula.net.NetMsgId;
-import emu.nebula.proto.GachaNewbieSpin.GachaNewbieSpinResp;
-import emu.nebula.proto.GachaSpin.GachaSpinReq;
-import emu.nebula.util.Utils;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
 import emu.nebula.net.HandlerId;
-import emu.nebula.data.GameData;
 import emu.nebula.net.GameSession;
 
 @HandlerId(NetMsgId.gacha_newbie_spin_req)
@@ -15,27 +10,7 @@ public class HandlerGachaNewbieSpinReq extends NetHandler {
 
     @Override
     public byte[] handle(GameSession session, byte[] message) throws Exception {
-        @SuppressWarnings("unused")
-        var req = GachaSpinReq.parseFrom(message);
-        
-        // Temp
-        var list = new IntArrayList();
-        
-        for (var d : GameData.getCharacterDataTable()) {
-            if (d.getGrade() == 1) {
-                list.add(d.getId());
-            }
-        }
-        
-        // 
-        var rsp = GachaNewbieSpinResp.newInstance();
-        
-        for (int i = 0; i < 10; i++) {
-            int id = Utils.randomElement(list);
-            rsp.addCards(id);
-        }
-        
-        return session.encodeMsg(NetMsgId.gacha_newbie_spin_succeed_ack, rsp);
+        return session.encodeMsg(NetMsgId.gacha_newbie_spin_failed_ack);
     }
 
 }
